54aa143f874390417f9f069a810314390a6d1230
Evili del Rio i Silvan <evili.del.rio@gmail.com>
2016-05-20 12:55:42 +0200

    Added Phylum model

--- clades/tests.py
+++ clades/tests.py
@@ -1,11 +1,19 @@
 from django.test import TestCase
 
-from .models import Kingdom
+from .models import Kingdom, Phylum
 
 _KINGDOM_NAME = 'Animalia'
+_PHYLUM_NAME = 'Chordata'
 
 class KingdomTest(TestCase):
     def test_kingdom_name(self):
         kingdom = Kingdom(name=_KINGDOM_NAME)
         kingdom.save()
         self.assertEqual(kingdom.name, _KINGDOM_NAME)
+
+class PhylumTest(TestCase):
+    def test_phylum_name(self):
+        phylum = Phylum(name=_PHYLUM_NAME)
+        phylum.save()
+        self.assertEqual(phylum.name, _PHYLUM_NAME)
+
