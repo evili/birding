7e0d77cf1131c15c658ede03a3dc3f7ab237a948
Evili del Rio i Silvan <evili.del.rio@gmail.com>
2016-05-20 13:17:59 +0200

    Correct test setUp

--- clades/tests.py
+++ clades/tests.py
@@ -12,18 +12,20 @@ class KingdomTest(TestCase):
         self.assertEqual(kingdom.name, _KINGDOM_NAME)
 
 class PhylumTest(TestCase):
+
+    def setUp(self):
+        self.kingdom = Kingdom(name=_KINGDOM_NAME)
+        self.kingdom.save()
+
     def test_phylum_name(self):
-        phylum = Phylum(name=_PHYLUM_NAME)
+        phylum = Phylum(name=_PHYLUM_NAME, kingdom=self.kingdom)
         phylum.save()
         self.assertEqual(phylum.name, _PHYLUM_NAME)
 
     def test_phylum_has_kingdom(self):
-        kingdom = Kingdom(name=_KINGDOM_NAME)
-        kingdom.save()
-        phylum = Phylum(name=_PHYLUM_NAME)
-        phylum.kingdom = kingdom
+        phylum = Phylum(name=_PHYLUM_NAME, kingdom=self.kingdom)
         phylum.save()
         # Retrieve object from DB
         phylum = Phylum.objects.get(name=_PHYLUM_NAME)
-        self.assertEqual(phylum.kingdom, kingdom)
+        self.assertEqual(phylum.kingdom, self.kingdom)
 
