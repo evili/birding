80f2b473968840e47480ddb34f46044f95d27406
Evili del Rio i Silvan <erio@iri.upc.edu>
2018-05-08 13:33:17 +0200

    Add ClaseTest

--- clades/tests.py
+++ clades/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 
-from .models import Kingdom, Phylum
+from .models import Kingdom, Phylum, Classe
 
 _KINGDOM_NAME = u'Animalia'
 _PHYLUM_NAME = u'Chordata'
+_CLASSE_NAME = u'Aves'
 
 
 class CladesTest(TestCase):
@@ -14,7 +15,10 @@ class CladesTest(TestCase):
         (self.phylum, created) = Phylum.objects.get_or_create(
             name=_PHYLUM_NAME,
             kingdom=self.kingdom)
-
+        (self.classe, created) = Classe.objects.get_or_create(
+            name=_CLASSE_NAME,
+            phylum=self.phylum
+            )
 
 class KingdomTest(CladesTest):
     def test_kingdom_name(self):
@@ -28,3 +32,8 @@ class PhylumTest(CladesTest):
     def test_phylum_has_kingdom(self):
         phylum = Phylum.objects.get(name=_PHYLUM_NAME)
         self.assertEqual(phylum.kingdom, self.kingdom)
+
+class ClasseTest(CladesTest):
+    def test_classe_has_phylum(self):
+        classe = Classe.objects.get(name=_CLASSE_NAME)
+        self.assertEqual(classe.phylum, self.phylum)
