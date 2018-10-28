7c2b3eda65a0ae2eb6a743143b80d01d5f83174f
Evili del Rio i Silvan <evili.del.rio@gmail.com>
2018-10-28 21:03:12 +0100

    Add login/logut and friends views

---
+++ birding/tests.py
@@ -0,0 +1,46 @@
+import unittest
+from django.test import Client
+from django.urls import reverse
+
+
+class AuthUrlsTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def visit_view(self, name, follow=True, kwargs=None):
+        return self.client.get(reverse(name, kwargs=kwargs), follow=follow)
+
+    def test_login(self):
+        resp = self.visit_view('login')
+        self.assertEqual(200, resp.status_code, "No login page")
+
+    def test_logout(self):
+        resp = self.visit_view('logout')
+        self.assertEqual(200, resp.status_code, "No logout page")
+
+    # def test_password_change(self):
+    #     resp = self.visit_view('password_change')
+    #     self.assertEqual(200, resp.status_code, "No password change page")
+
+    # def test_password_change_done(self):
+    #     resp = self.visit_view('password_change_done')
+    #     self.assertEqual(200, resp.status_code, "No password change done page")
+
+    def test_password_reset(self):
+        resp = self.visit_view('password_reset')
+        self.assertEqual(200, resp.status_code, "No password reset page")
+    
+    def test_password_reset_done(self):
+        resp = self.visit_view('password_reset_done')
+        self.assertEqual(200, resp.status_code, "No password reset-done page")
+
+    def test_password_reset_confirm(self):
+        resp = self.visit_view('password_reset_confirm', kwargs={
+            'uidb64':'9876543210',
+            'token': '0123456789-0123456789',
+        })
+        self.assertEqual(200, resp.status_code, "No reset-confirm page")
+
+    def test_password_reset_complete(self):
+        resp = self.visit_view('password_reset_complete')
+        self.assertEqual(200, resp.status_code, "No reset-done page")
